eadde9c0f278afdb1301fa6d075d2367373c4150
Kenpal-HarunaOka <h.oka@kenpalinc.com>
2025-04-15 18:09:32 +0900

    fix: change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
       </h1>
       <div className="flex gap-4 items-center justify-center">
         <Button colorGray>NEGATIVE</Button>
-        <Button colorBlue>ACTIVE</Button>
+        <Button colorBlue>POSITIVE</Button>
         <Button colorRed>DELETE</Button>
         <Button colorGreen>UPDATE</Button>
         <Button className="bg-purple-500 hover:bg-purple-600 rounded-2xl px-10 py-6">
